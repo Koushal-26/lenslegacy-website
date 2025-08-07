f30a72c3a6bca5fc861a078056f780367b52e1a3
Koushal-26 <sharmajijkjk@gmail.com>
2025-08-07 15:05:32 +0530

    initial commit

--- app/components/About.js
+++ app/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
   <div className="container mx-auto px-6 grid md:grid-cols-2 gap-12 items-center transform-none">
    <div>
     <Image priority={true}
-    src="/artificial-intelligence.jpg"
+    src="/Artificial-intelligence.jpg"
      alt="Portrait of a Standing Robart (AI)"
      width={500}
      height={500}
